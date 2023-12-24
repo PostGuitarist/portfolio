4a67e4daa7105a0fc13119a201612cffb4eca198
Zaden Connell <zaden.connell@gmail.com>
2023-12-23 22:01:07 -0500

    Reinit npm packages to be fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
   markdown: {
-    syntaxHighlight: true,
+    syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "nord",
       wrap: false,
     },
   },
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
