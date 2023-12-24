7ca20df461b7009a0657a0ab22ad2cc8b245f5ee
Zaden Connell <zaden.connell@gmail.com>
2023-12-23 20:35:04 -0500

    Update ESLint and Astro configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
+    extends: ["plugin:astro/recommended"],
+    overrides: [
         {
-            "env": {
-                "node": true
+            files: ["*.astro"],
+            parser: "astro-eslint-parser",
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                extraFileExtensions: [".astro"],
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            rules: {},
+        },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+};
